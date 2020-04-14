92e5d293fc6168c563e4d3605737eeaf84caf003
doujadouieb <63654623+doujadouieb@users.noreply.github.com>
2020-04-14 15:39:42 +0100

    Add files via upload

---
+++ decouvrez-nous.php
@@ -0,0 +1,96 @@
+<html>
+<head>
+<title>Musées</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />
+<link rel="file:///C:/MAMP/htdocs/Musees/Accueil.php" />
+</head>
+<body>
+<div id ="banner1">
+<p>MUSEE DE FRANCE</p>
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+
+
+<div>
+<center>
+<ul>
+<li><a href="Accueil.php">Accueil</a></li>
+<li><a href="muse.php">Nos Musées</a></li>
+<li><a href="decouvrez-nous.php">Découvrez-Nous </a></li>
+<li><a href="../musees/login/login.php.php">Se Déconnecter </a></li>
+</ul>
+</center>
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+  
+    
+	<div>
+	<div>
+	<h1><center>Découvrez nos musées selon la région où vous allez !</center></h1></div>
+	<br> <br> <br> <br>
+	<link rel="stylesheet" href="style/decouvrez-nous.css" type="text/css" media="screen" /><center>
+	<div id="db">
+	<h2>Musée de France :</h2>
+	<h4>Lors d’une visite au musée, il y a deux types de personnes :</h4><h4> ceux qui subissent et les passionnés. Toutefois, certains musées parviennent à mettre tout le monde d’accord. 
+	Ils sont des témoignages de culture, d’art ou encore d’histoire, ils nous impressionnent,
+	ils sont sur la liste des choses à visiter de milliers de touristes ; les musées sont des petits mondes à part où il fait bon se cultiver, regarder, admirer et se questionner. </h4>
+	<h4> L'objectif de notre site est faciliter la recherche des musées selon la région où vous allez.</h4>
+	<h4>Pour plus d'informations, vous pouvez nous contacter :</h4>
+	<p>Email : musée.france@gmail.com</p>
+	<p>Téléphone : 06.99.45.80.03</p></div></center>
+	
+	
+  <br>
+<br>
+<br>
+<br>
+<br><hr>
+<br>
+<br>
+<br>
+<br>
+  <center>
+<div><h3>Les musées sont souvent spécialisés, il en existe principalement neuf grandes catégories :</h3><br><br>
+<p>les musées d'archéologie || les musées d'art || les Musée des Beaux-Arts || les musées des arts décoratifs || les musées d'histoire || les musées de sciences || musées d'histoire naturelle || les musées des techniques |
+les musées d'ethnologie. 
+
+</p></center>
+
+<br>
+<br>
+<br>
+<br>
+
+<footer>
+<center>
+<p><b>MUSEE DE FRANCE</b></p>
+<a href="decouvrez-nous.php">Contactez-nous</a>
+<p><a href="Accueil.php" >Retour </a></p>
+</center>
+</footer
+</body>
+</html>
