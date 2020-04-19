53e43c823f41656fc3cce6321ae1ed4dd52cb843
doujadouieb <63654623+doujadouieb@users.noreply.github.com>
2020-04-19 13:32:29 +0000

    Add files via upload

---
+++ occitanie.php
@@ -0,0 +1,119 @@
+
+<html>
+<head>
+
+<title>Musées</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />
+<link rel="file:///C:/MAMP/htdocs/Musees/Accueil.php" />  
+<link rel="file:///C:/MAMP/htdocs/Musees/musee.php" />  
+
+<div id ="banner1">
+<p>MUSEE DE FRANCE</p>
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div>
+
+<ul>
+<center>
+<li><a href="Accueil.php">Accueil</a></li>
+<li><a href="musee.php">Nos Musées</a></li>
+<li><a href="decouvrez-nous.php">Découvrez-Nous </a></li>
+<li><a href="../login/login.php">Se Déconnecter </a></li>
+</center>
+</ul>
+
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+  
+ <div>
+ <h1><center>Découvrez nos musées selon la région où vous allez !</center></h1></div>
+ <br>
+<br>
+<br>
+ <h2><center> les Musées en Occitanie</center></h2></div>
+ 
+<hr>
+<br>
+<br>
+<div id="text">
+ <h3><center>  L’Occitanie est une région culturelle et historique du sud-ouest de l’Europe. <BR>
+ Les principaux éléments qui la caractérisent sont sa culture et sa langue, la langue d'oc qui lui a donné son nom. <BR>
+ L’espace occitan est identifié dès l’Empire romain sous le nom de Viennoise ou de Sept-Provinces, et au début du Moyen Âge sous le nom d’Aquitaine.<BR>
+ Il retrouve une certaine unité à l'époque carolingienne sous le nom de royaume d'Aquitaine dont les délimitations territoriales correspondent grosso modo à l'espace linguistique de l'ancien occitan et de la Viennoise romaine.
+</center></h3></div>
+<br>
+<br>
+
+<hr>
+<br>
+<br>
+<br>
+
+<div id="img">
+<center>
+<table>
+<tr>
+<td><a><img SRC="image/O1.png" width="600"  height ="350"></a></td>
+<td><a><img SRC="image/O2.png" width="600"  height ="350"></a></td>
+</center>
+<br>
+<center>
+<table>
+<tr>
+<td><a><img SRC="image/O3.png" width="600"  height ="350"></a></td>
+<td><a><img SRC="image/O4.png" width="600"  height ="350"></a></td>
+</tr>
+</table>
+</center>
+</div>
+<br>
+
+<br>
+<br>
+<br>
+<br>  
+<hr><br>
+<br>
+<br><center>
+<div><h3>Les musées sont souvent spécialisés, il en existe principalement neuf grandes catégories :</h3><br><br>
+<p>les musées d'archéologie || les musées d'art || les Musée des Beaux-Arts || les musées des arts décoratifs || les musées d'histoire || les musées de sciences || musées d'histoire naturelle || les musées des techniques |
+les musées d'ethnologie. 
+
+</p></center>
+<br>
+<br>
+<br>
+
+
+
+<footer>
+<center>
+<p><b>MUSEE DE FRANCE</b></p>
+<a href="decouvrez-nous.php">Contactez-nous</a>
+<p><a href="Accueil.php" >Retour </a></p>
+</center>
+</
