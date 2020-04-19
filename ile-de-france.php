78822bf21201495d4f0c6d50b0995830c9d2108c
doujadouieb <63654623+doujadouieb@users.noreply.github.com>
2020-04-19 13:31:33 +0000

    Add files via upload

---
+++ ile-de-france.php
@@ -0,0 +1,121 @@
+<html>
+<head>
+
+<title>Musées</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />
+<link rel="file:///C:/MAMP/htdocs/Musees/Accueil.php" />  
+<link rel="file:///C:/MAMP/htdocs/Musees/musee.php" />  
+
+<div id ="banner1">
+<p>MUSEE DE FRANCE</p>
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div>
+
+<ul>
+<center>
+<li><a href="Accueil.php">Accueil</a></li>
+<li><a href="musee.php">Nos Musées</a></li>
+<li><a href="decouvrez-nous.php">Découvrez-Nous </a></li>
+<li><a href="login.php">Se Déconnecter </a></li>
+</center>
+</ul>
+
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+  
+ <div>
+ <h1><center>Découvrez nos musées selon la région où vous allez !</center></h1></div>
+ <br>
+<br>
+<br>
+ <h2><center> les Musées en iLE-DE-France </center></h2></div>
+ 
+<hr>
+<br>
+<br>
+<div id="text">
+ <h3><center>Du plus classique au plus original, la région parisienne regorge de musées à découvrir.    <br>
+ Vivez un moment de divertissement et de culture seul, entre amis ou en famille.   <br>
+
+</center></h3></div>
+<br>
+<br>
+
+<hr>
+<br>
+<br>
+<br>
+
+<div id="img">
+<center>
+<table>
+<tr>
+<td><a><img SRC="image/i1.png" width="200"  height ="350"></a></td>
+<td><a><img SRC="image/i2.png" width="200"  height ="350"></a></td>
+</center>
+<br>
+<center>
+<table>
+<tr>
+<td><a><img SRC="image/i3.png" width="200"  height ="350"></a></td>
+<td><a><img SRC="image/i4.png" width="200"  height ="350"></a></td>
+</tr>
+</table>
+</center>
+</div>
+<br>
+
+<br>
+<br>
+<br>
+<br>  
+<hr><br>
+<br>
+<br><center>
+<div><h3>Les musées sont souvent spécialisés, il en existe principalement neuf grandes catégories :</h3><br><br>
+<p>les musées d'archéologie || les musées d'art || les Musée des Beaux-Arts || les musées des arts décoratifs || les musées d'histoire || les musées de sciences || musées d'histoire naturelle || les musées des techniques |
+les musées d'ethnologie. 
+
+</p></center>
+<br>
+<br>
+<br>
+
+
+
+<footer>
+<center>
+<p><b>MUSEE DE FRANCE</b></p>
+<a href="decouvrez-nous.php">Contactez-nous</a>
+<p><a href="Accueil.php" >Retour </a></p>
+</center>
+</footer>
+
+ 
+</body>
+</html>
