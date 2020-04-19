d346d755bb49d3fa0098a0fa5f1cc2e7f8608f31
doujadouieb <63654623+doujadouieb@users.noreply.github.com>
2020-04-19 13:26:23 +0000

    Add files via upload

---
+++ bretagne.php
@@ -0,0 +1,117 @@
+<html>
+<head>
+
+<title>Musées</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />
+<link rel="file:///C:/MAMP/htdocs/Musees/Accueil.php" />  
+<link rel="file:///C:/MAMP/htdocs/Musees/musee.php" />  
+
+<div id ="banner1">
+<p>MUSEE DE FRANCE</p>
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div>
+
+<ul>
+<center>
+<li><a href="Accueil.php">Accueil</a></li>
+<li><a href="musee.php">Nos Musées</a></li>
+<li><a href="decouvrez-nous.php">Découvrez-Nous </a></li>
+<li><a href="../login/login.php">Se Déconnecter </a></li>
+</center>
+</ul>
+
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+  
+ <div>
+ <h1><center>Découvrez nos musées selon la région où vous allez !</center></h1></div>
+ <br>
+<br>
+
+<br>
+ <h2><center> les Musées en Bretagne  </center></h2></div>
+ 
+<hr>
+<br>
+<br>
+<div id="text">
+ <h3><center>  La Bretagne est une entité géographique et culturelle,<br> et une des nations dites celtiques. 
+ Elle occupe une péninsule, à l'extrémité ouest de la France, <br>située entre la Manche au nord, la mer Celtique
+ et la mer d'Iroise à l'ouest et le golfe de Gascogne au sud.
+
+</center></h3></div>
+<br>
+<br>
+
+<hr>
+<br>
+<br>
+<br>
+
+<div id="img">
+<center>
+<table>
+<tr>
+<td><a><img SRC="image/bretagne1.png" width="200"  height ="350"></a></td>
+<td><a><img SRC="image/bretagne2.png" width="200"  height ="350"></a></td>
+<td><a><img SRC="image/bretagne3.png" width="200"  height ="350"></a></td>
+</tr>
+</table>
+</center>
+</div>
+<br>
+
+<br>
+<br>
+<br>
+<br>  
+<hr><br>
+<br>
+<br><center>
+<div><h3>Les musées sont souvent spécialisés, il en existe principalement neuf grandes catégories :</h3><br><br>
+<p>les musées d'archéologie || les musées d'art || les Musée des Beaux-Arts || les musées des arts décoratifs || les musées d'histoire || les musées de sciences || musées d'histoire naturelle || les musées des techniques |
+les musées d'ethnologie. 
+
+</p></center>
+<br>
+<br>
+<br>
+
+
+
+<footer>
+<center>
+<p><b>MUSEE DE FRANCE</b></p>
+<a href="decouvrez-nous.php">Contactez-nous</a>
+<p><a href="Accueil.php" >Retour </a></p>
+</center>
+</footer>
+
+ 
+</body>
+</html>
